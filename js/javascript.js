979a39d64bd58c69d371a37c779ae4d33b09b63f
ivan <example@mail.com>
2025-04-09 23:27:07 +0300

    add personalMovieDB

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,33 @@
 'use strict';
 
-console.log('arr' + ' - object');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
-console.log(4 + +' - object');
+
+const personalMovieDB = {
+     'count' : numberOfFilms,
+     'movies' : {},
+     'actor' : {} ,
+     'genres' : [] ,
+     'privat' : false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+
+
+console.log(personalMovieDB);
+
+
+
+
+
+
